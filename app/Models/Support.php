746d28d73666e1a05e0ea7d3779c0042eaa00208
Álefe Alves <alefealves@hotmail.com>
2024-04-13 18:13:51 -0400

    feat:  Usando ENUMs

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -2,16 +2,28 @@
 
 namespace App\Models;
 
+use App\Enum\SupportStatus;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Support extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = [
-      'subject',
-      'body',
-      'status'
-    ];
+  protected $fillable = [
+    'subject',
+    'body',
+    'status'
+  ];
+
+  // protected $casts = [
+  //   'status' => SupportStatus::class,
+  // ];
+  public function status(): Attribute
+  {
+    return Attribute::make(
+      set: fn (SupportStatus $status) => $status->name,
+    );
+  }
 }
