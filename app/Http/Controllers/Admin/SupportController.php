cc3875479703e37058e604628e5261da7ec4bb8f
Álefe Alves <alefealves@hotmail.com>
2024-04-12 21:26:04 -0400

    feat: trabalhando com DTOs

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
@@ -41,10 +43,9 @@ class SupportController extends Controller
 
   public function store(StoreUpdateSupport $request, Support $support)
   {
-    $data = $request->validated();
-    $data['status'] = 'a';
-
-    $support = $support->create($data);
+    $this->service->new(
+      CreateSupportDTO::makeFromRequest($request)
+    );
 
     return redirect()->route('supports.index');
   }
@@ -61,19 +62,13 @@ class SupportController extends Controller
 
   public function update(StoreUpdateSupport $request, Support $support, string $id)
   {
-    if (!$support = $support->find($id)) {
+    $this->service->update(
+      UpdateSupportDTO::makeFromRequest($request)
+    );
+
+    if (!$support) {
       return back();
     }
-
-    // $support->subject = $request->subject;
-    // $support->body = $request->body;
-    // $support->save();
-
-    // $support->update($request->only([
-    //   'subject', 'body'
-    // ]));
-
-    $support->update($request->validated());
 
     return redirect()->route('supports.index');
   }
