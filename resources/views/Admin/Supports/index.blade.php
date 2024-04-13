1e0c3b92a4c09160152387cbbbc7173610418714
Álefe Alves <alefealves@hotmail.com>
2024-04-12 22:31:57 -0400

    feat: organizar views com componentes

--- resources/views/Admin/Supports/index.blade.php
+++ resources/views/Admin/Supports/index.blade.php
@@ -12,12 +12,12 @@
   <tbody>
     @foreach ($supports as $support)
       <tr>
-        <th>{{ $support->subject }}</th>
-        <th>{{ $support->status }}</th>
-        <th>{{ $support->body }}</th>
+        <th>{{ $support['subject'] }}</th>
+        <th>{{ $support['status'] }}</th>
+        <th>{{ $support['body'] }}</th>
         <td>
-          <a href="{{ route('supports.show', [$support->id]) }} ">ir</a>
-          <a href="{{ route('supports.edit', [$support->id]) }} ">editar</a>
+          <a href="{{ route('supports.show', $support['id']) }} ">ir</a>
+          <a href="{{ route('supports.edit', $support['id']) }} ">editar</a>
         </td>
       </tr>
     @endforeach
