d76b8bcdf09838b23d19df4c105e9b8f7ab05093
Álefe Alves <alefealves@hotmail.com>
2024-04-12 21:45:31 -0400

    feat: trabalhando com Repository Pattern e Inversão de Dependência

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,27 @@
 
 namespace App\Providers;
 
+use App\Repositories\SupportRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
+use SupportEloquentORM;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    $this->app->bind(
+      SupportRepositoryInterface::class,
+      SupportEloquentORM::class
+    );
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+  }
 }
