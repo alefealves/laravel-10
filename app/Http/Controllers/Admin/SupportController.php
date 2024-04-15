cd2285c721ccda0911c1b3d9cbf69cc6a24a4f86
Álefe Alves <alefealves@hotmail.com>
2024-04-15 09:32:45 -0400

    feat: Autenticação no Laravel 10

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -21,7 +21,7 @@ class SupportController extends Controller
   {
     $supports = $this->service->paginate(
       page: $request->get('page', 1),
-      totalPerPage: $request->get('per_page', 15),
+      totalPerPage: $request->get('per_page', 6),
       filter: $request->filter
     );
 
@@ -53,7 +53,9 @@ class SupportController extends Controller
       CreateSupportDTO::makeFromRequest($request)
     );
 
-    return redirect()->route('supports.index');
+    return redirect()
+      ->route('supports.index')
+      ->with('message', 'Cadastrado com sucesso!');
   }
 
   public function edit(string $id)
@@ -76,13 +78,17 @@ class SupportController extends Controller
       return back();
     }
 
-    return redirect()->route('supports.index');
+    return redirect()
+      ->route('supports.index')
+      ->with('message', 'Atualizado com sucesso!');
   }
 
   public function destroy(string $id)
   {
     $this->service->delete($id);
 
-    return redirect()->route('supports.index');
+    return redirect()
+      ->route('supports.index')
+      ->with('message', 'Deletado com sucesso!');
   }
 }
